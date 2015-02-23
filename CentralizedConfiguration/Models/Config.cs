c60aafc1bf65f553eee9e2bdfc5e6077daa36f83
StephRagazzi <stephane.ragazzi@gmail.com>
2015-02-23 08:52:51 +0100

    Model classes added to project

---
+++ CentralizedConfiguration/Models/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace CentralizedConfiguration.Models
+{
+    public class Config
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public XmlDocument Config { get; set; }
+    }
+}
---
+++ CentralizedConfiguration/Models/Site.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CentralizedConfiguration.Models
+{
+    public class Site
+    {
+        public string Name { get; set; }
+        public List<Config> Configs { get; set; }
+    }
+}
